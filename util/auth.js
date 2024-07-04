860a2e0546f8e19d1791086c5808c49fc8a6da1f
Buiduc03 <116989742+Buiduc03@users.noreply.github.com>
2024-07-04 14:40:30 +0700

    Update auth.js
    Fix security alert

--- util/auth.js
+++ util/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = 'AIzaSyBae9o1bR-Rm3xlV74ON2VVTuHhf9dHxU8';
+const API_KEY = 'API_KEY';
 
 async function authenticate(mode, email, password) {
   const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
